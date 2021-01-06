bbd3fa5204053659a4e7a42183201a002564d4a6
minnji88 <kkhhjj3393@gmail.com>
2021-01-06 13:42:57 +0900

    github

---
+++ main.js
@@ -0,0 +1,25 @@
+(function (window, document){
+    'use strict';
+    const $toggles = document.querySelectorAll('.toggle'); /* 선택자로 찾는 개념 */ // NodeList
+    const $toggleBtn = document.getElementById('toggle-btn'); /* ID 로 찾는 개념 */
+    $toggleBtn.addEventListener('click', function(){
+        toggleElements();
+    });
+    window.addEventListener('resize', function(){
+        if(window.innerWidth > 1024){
+            // Off toggle element
+            offElements();
+        }
+    });
+    function toggleElements(){ // 보이고 사라지는 개념
+        [].forEach.call($toggles, function(toggle){
+            toggle.classList.toggle('on'); // 토글이란 클래스 선택자를 on 이란 클래스를 지속적으로 토글
+        });
+    }
+    
+    function offElements(){ // 사라지는 개념
+        [].forEach.call($toggles, function(toggle){
+            toggle.classList.remove('on'); // 토글이란 클래스 선택자를 on 이란 클래스를 지속적으로 토글
+        });
+    }
+})(window, document)
